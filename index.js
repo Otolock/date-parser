70b275652358433ab772304de23b1a47e896264e
Antonio Santos <aj.santos128@gmail.com>
2019-01-20 18:07:15 -0400

    initial commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+var moment = require('moment');
+
+var daysOfTheWeek = /\b((mon|tues|wed(nes)?|thur(s)?|fri|sat(ur)?|sun)(day)?)\b/g;
+var currentWeek= /\b(on|this)\b/gi
+
+var testSentence = process.argv[2] || "On monday";
+
+if (currentWeek.test(testSentence)) {
+	let date = moment().day(testSentence.match(daysOfTheWeek)[0]);
+
+	console.log(date.format("dddd, MMMM Do YYYY"));
+}
