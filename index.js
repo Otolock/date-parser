7dc64a2f9cb2fb9a48188d5e0951263f9c38d3e8
Antonio Santos <aj.santos128@gmail.com>
2019-01-20 20:36:53 -0400

    Parse relative dates for current and future weeks

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var moment = require('moment');
 
 var daysOfTheWeek = /\b((mon|tues|wed(nes)?|thur(s)?|fri|sat(ur)?|sun)(day)?)\b/g;
-var currentWeek= /\b(on|this)\b/gi
+var currentWeek = /\b(on|this)\b/gi
+var nextWeek = /\b(next|following)\b/gi
 
 var testSentence = process.argv[2] || "On monday";
 
@@ -9,4 +10,10 @@ if (currentWeek.test(testSentence)) {
 	let date = moment().day(testSentence.match(daysOfTheWeek)[0]);
 
 	console.log(date.format("dddd, MMMM Do YYYY"));
+}
+
+if (nextWeek.test(testSentence)) {
+	let date = moment().day(testSentence.match(daysOfTheWeek)[0]).add(7, 'days');
+
+	console.log(date.format('dddd, MMMM Do YYYY'));
 }
