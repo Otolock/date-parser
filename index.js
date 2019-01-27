cc0859793d257bc8682b253d967171e8bcdb85ea
Antonio Santos <aj.santos128@gmail.com>
2019-01-27 00:49:15 -0400

    Refactor code into function parseDate

--- index.js
+++ index.js
@@ -13,19 +13,27 @@ function parseDate(sentence) {
 	// Format to be used when returning date information
 	const DATE_FORMAT = 'dddd, MMMM Do YYYY';
 
-	if (date.diff(moment()) < 0) {
-		date.add(7, 'days');
+	if (/\b(today)\b/gi.test(sentence)) {
+		return moment().format(DATE_FORMAT);
 	}
 
-	console.log(date.format("dddd, MMMM Do YYYY"));
+	// Match the day of the week from the sentence. 
+	var day = sentence.match(daysOfTheWeek)[0] || undefined;
 
-} else if (/\b(today)\b/gi.test(testSentence)) {
-	let date = moment();
+	if (!day) {
+		throw new Error('No day of the week specified.');
+	}
 
-	console.log(date.format('dddd, MMMM Do YYYY'));
-
-} else if (nextWeek.test(testSentence)) {
-	let date = moment().day(testSentence.match(daysOfTheWeek)[0]).add(7, 'days');
-
-	console.log(date.format('dddd, MMMM Do YYYY'));
+	if (currentWeek.test(sentence)) {
+		var date = moment().day(day);
+		
+		if (date.diff(moment()) < 0) {
+			date.add(7, 'days');
+			return date.format(DATE_FORMAT);
+		} else {
+			return date.format(DATE_FORMAT);
+		}
+	} else if (nextWeek.test(sentence)) {
+		return moment().day(day).add(7, 'days').format(DATE_FORMAT);
+	}
 }
