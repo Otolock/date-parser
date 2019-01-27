fb219dcb4b4990f68bc197ae994a301e8efe5f5b
Antonio Santos <aj.santos128@gmail.com>
2019-01-27 00:48:39 -0400

    Comments for regex used.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var moment = require('moment');
 
+// Regex for matching days of the week.
 var daysOfTheWeek = /\b((mon|tues|wed(nes)?|thur(s)?|fri|sat(ur)?|sun)(day)?)\b/gi;
+
+// Regex for testing if sentence refers to the current week.
 var currentWeek = /\b(on|this)\b/gi
+
+// Regex for testing if sentence refers to the next week.
 var nextWeek = /\b(next|following)\b/gi
 
-var testSentence = process.argv[2] || "On monday";
-
-if (currentWeek.test(testSentence)) {
-	let date = moment().day(testSentence.match(daysOfTheWeek)[0]);
+function parseDate(sentence) {
+	// Format to be used when returning date information
+	const DATE_FORMAT = 'dddd, MMMM Do YYYY';
 
 	if (date.diff(moment()) < 0) {
 		date.add(7, 'days');
